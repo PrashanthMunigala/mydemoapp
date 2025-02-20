0c32aa5197b421901bf97abe92cea5c2faa5d4d4
PrashanthMunigala <munigalaprashanth57555@gmail.com>
2025-02-20 11:52:12 +0530

    Update main.tf

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -20,7 +20,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./eks-install/vpc"
 
   vpc_cidr             = var.vpc_cidr
   availability_zones   = var.availability_zones
@@ -30,7 +30,7 @@ module "vpc" {
 }
 
 module "eks" {
-  source = "./modules/eks"
+  source = "./eks-install/eks"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
