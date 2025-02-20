4d08217168cca767e3525e2bf69481fabeea2191
PrashanthMunigala <munigalaprashanth57555@gmail.com>
2025-02-20 12:12:00 +0530

    Create main.tf

---
+++ eks-install/backend/main.tf
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+    parameters {
+        choice(name: 'ACTION', choices: ['apply', 'destroy'], description: 'Select the action')
+    }
+    stages {
+        stage('git clone') {
+            steps {
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/PrashanthMunigala/mydemoapp.git']])
+            }
+        }
+        stage('build backend') {
+            steps {
+                    sh 'terraform init'
+                }
+        }
+        stage('backend terraform apply or destroy') {
+            steps {
+                script {
+                    if (params.ACTION == 'apply') {
+                            sh 'terraform apply'
+                    } 
+                    else if (params.ACTION == 'destroy') {
+                            sh 'terraform destroy'
+                    }
+                }
+            }
+        }
+    }
+}
